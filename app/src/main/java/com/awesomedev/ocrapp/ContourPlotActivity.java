198c2ff3aecdd1a8fec14698bb1f179e9010e260
Sparsh Bansal <sparsh.bansal17895@gmail.com>
2018-01-02 19:04:55 +0530

    Implemented MSER

---
+++ app/src/main/java/com/awesomedev/ocrapp/ContourPlotActivity.java
@@ -0,0 +1,13 @@
+package com.awesomedev.ocrapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContourPlotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contour_plot);
+    }
+}
---
+++ app/src/main/java/com/awesomedev/ocrapp/ContourRect.java
@@ -0,0 +1,9 @@
+package com.awesomedev.ocrapp;
+
+/**
+ * Created by sparsh on 12/29/17.
+ */
+
+public class MPoint {
+    public double x,y;
+}
